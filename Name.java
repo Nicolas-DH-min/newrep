cdd4fcedb8cf806bc42d60f69b377ddf5ef4ad4b
unknown <dddfffggg@gmail.com>
2025-01-15 19:47:48 +0300

    Add Name.java class

---
+++ Name.java
@@ -0,0 +1,10 @@
+public class Name {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        printName("John Doe");
+    }
+
+    public static void printName(String name) {
+        System.out.println("Name: " + name);
+    }
+}
